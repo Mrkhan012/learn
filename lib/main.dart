f795622d06230b57527efeedccbac121d7f1847b
Mrkhan012 <afroz.p8111@gmail.com>
2023-02-17 14:49:38 +0530

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+// ignore_for_file: equal_keys_in_map
+
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
+
+import 'pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,13 +14,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Material(
-        child: Center(
-          child: Text("Welcome to 30 my lite word invert"),
-        ),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
       ),
+      initialRoute: "/login",
+      routes: {
+        "/": (context) => const LoginPage(),
+        "/home": (context) => const HomePage(),
+        "/login": (context) => const LoginPage()
+      },
     );
   }
 }
